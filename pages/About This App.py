3be4ae283aab73c148c439d7a6bfc6bb08d6f07d
kwonsungja <4gg4g4h4@gmail.com>
2024-12-16 17:04:16 +0900

    Update About This App.py

--- pages/About This App.py
+++ pages/About This App.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("About This App")
-st.write("# To improve about basic rules for forming regular plural. First add -s to the singular noun, Second add -es to nouns ending in -s, -ss, -sh, -ch, -x, -z, or -o. Last if the -y is preceded by a consonant, change -y to -ies. Students sometimes may not know how to use -s, -es, or -ies after singular nouns. By learning these rules, they can resolve noun agreement issues and improve their understanding of nouns. Therefore, by utilizing this app, learners can be encouraged to participate, boosting their motivation and interest in learning English.")
+st.write("To improve about basic rules for forming regular plural. First add -s to the singular noun, Second add -es to nouns ending in -s, -ss, -sh, -ch, -x, -z, or -o. Last if the -y is preceded by a consonant, change -y to -ies. Students sometimes may not know how to use -s, -es, or -ies after singular nouns. By learning these rules, they can resolve noun agreement issues and improve their understanding of nouns. Therefore, by utilizing this app, learners can be encouraged to participate, boosting their motivation and interest in learning English.")
